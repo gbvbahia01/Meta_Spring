4d4def5b8a8c063a8859be564a1d520afbfbca72
Guilherme Bahia <gbvbahia01@gmail.com>
2017-08-27 10:35:43 -0300

    postgres SQL create tables

--- DB.sql
+++ DB.sql
@@ -8,6 +8,57 @@
  * Created: Oct 17, 2016
  */
 
+--Postgres SQL
+CREATE TABLE "DEPARTMENT" (
+  "ID" bigserial NOT NULL,
+  "NAME" varchar(45) DEFAULT NULL,
+   "DESCRIPTION" varchar(45) DEFAULT NULL,
+  PRIMARY KEY ("ID")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "DEPARTMENT"
+  OWNER TO postgres;
+
+  CREATE TABLE "USER" (
+  "ID" bigserial NOT NULL,
+  "NAME" varchar(45) DEFAULT NULL,
+  "DESCRIPTION" varchar(45) DEFAULT NULL,
+  "DEPT_ID" bigint DEFAULT NULL,
+  PRIMARY KEY ("ID"),
+  foreign key("DEPT_ID") references "DEPARTMENT"("ID")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "USER"
+  OWNER TO postgres;
+
+CREATE TABLE "PERMISSION" (
+  "ID" bigserial NOT NULL,
+  "NAME" varchar(45) DEFAULT NULL,
+  "DESCRIPTION" varchar(45) DEFAULT NULL,
+  PRIMARY KEY ("ID")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "PERMISSION"
+  OWNER TO postgres;
+
+CREATE TABLE "USER_PERMISSION" (
+  "ID" bigserial NOT NULL,
+  "USER_ID" bigint DEFAULT NULL,
+  "PERM_ID" bigint DEFAULT NULL,
+  PRIMARY KEY ("ID"),
+  foreign key("USER_ID") references "USER"("ID"),
+  foreign key("PERM_ID") references "PERMISSION"("ID")
+)WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "USER_PERMISSION"
+  OWNER TO postgres;
+
+
+-- MYSQL
 CREATE TABLE `DEPARTMENT` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(45) DEFAULT NULL,
